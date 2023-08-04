33bb1c5df3588dafe5e0c7b6cbfd80bf095353ca
mrbrunelli <matheus.brunelli@gmail.com>
2023-08-03 21:11:42 -0300

    configure express app with passport

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import logger from "morgan";
+import passport from "passport";
+import { jwtStrategy } from "./passport/jwt-strategy";
+import { localStrategy } from "./passport/local-strategy";
+import auth from "./routes/auth";
+import user from "./routes/user";
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+const app = express();
+
+app.use(express.json());
+app.use(logger("dev"));
+
+app.use("/auth", auth);
+app.use("/user", passport.authenticate("jwt", { session: false }), user);
+
+export default app;
